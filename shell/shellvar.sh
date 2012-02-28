5fa2a0557f1fd05cdcefcdd7ea4ae098e790877a
lifuxian <lifuxianoa@163.com>
2012-02-28 15:48:42 +0800

    Add: vim config.

---
+++ shell/shellvar.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo $#
+echo $?
+echo $0
+echo $*
+echo $@
+echo $1
+echo $2
+
+echo "PATH=$PATH"
+echo "HOME=$HOME"
+echo "HISTSIZE=$HISTSIZE"
+echo "LOGNAME=$LOGNAME"
+echo "SHELL=$SHELL"
+echo "LANGUAGE=$LANGUAGE"
+echo "MAIL=$MAIL"
+echo "PS1=$PS1"
+echo "PS2=$PS2"
+echo "TERM=$TERM"
+echo "PWD=$PWD"
