757d45ae4f2b137351bf19d2aa2d171fb16c15c7
lifuxian <lifuxianoa@163.com>
2012-04-25 14:38:28 +0800

    modify: globalmem驱动例子简单实现

--- linux_driver/globalmem/globalmem.c
+++ linux_driver/globalmem/globalmem.c
@@ -31,25 +31,29 @@
 #define GLOBALMEM_SIZE  0X1000  /* 全局内存大小： 4KB */
 #define MEM_CLEAR   0X1 /* 清零全局内存 */
 #define GLOBALMEM_MAJOR 254 /* 预设的globalmem的主设备号 */
+#define GLOBALMEM_NUM   2   /* 全局内存个数 */
 
-static int globalmem_major = GLOBALMEM_MAJOR;
+//static int globalmem_major = GLOBALMEM_MAJOR;
+static int globalmem_major = 0;
 /* globalmem 设备结构体 */
 struct globalmem_dev
 {
     struct cdev cdev;   /* cdev结构体 */
     unsigned char mem[GLOBALMEM_SIZE];  /* 全局内存 */
-} dev;
+} *globalmem_devp;
 
 static ssize_t globalmem_read(struct file *filp, char __user *buf, 
         size_t count, loff_t *ppos)
 {
+    struct globalmem_dev *devp = filp->private_data;
     unsigned long p = *ppos;
     int ret = 0;
 
+    printk(KERN_INFO "----read %d bytes from %lu\n", count, p);
     /* 分析和获取有效的读长度 */
     if (p >= GLOBALMEM_SIZE)    /*要读的偏移位置越界*/
     {
-        return count ? -ENXIO : 0;
+        return count ? - ENXIO : 0;
     }
 
     if (count > GLOBALMEM_SIZE - p) /* 要读的字节数太大 */
@@ -58,7 +62,7 @@ static ssize_t globalmem_read(struct file *filp, char __user *buf,
     }
 
     /* 内核空间->用户空间 */
-    if (copy_to_user(buf, (void *)(dev.mem + p), count))
+    if (copy_to_user(buf, (void *)(devp->mem + p), count))
     {
         ret = -EFAULT;
     }
@@ -76,11 +80,13 @@ static ssize_t globalmem_read(struct file *filp, char __user *buf,
 static ssize_t globalmem_write(struct file *filp, const char __user *buf,
         size_t count, loff_t *ppos)
 {
+    struct globalmem_dev *devp = filp->private_data;
     unsigned long p = *ppos;
     int ret = 0;
 
+    printk(KERN_INFO "----write %d bytes to %lu\n", count, p);
     /* 分析和获取有效的写长度 */
-    if (p > GLOBALMEM_SIZE) /* 要写的偏移位置越界 */
+    if (p >= GLOBALMEM_SIZE) /* 要写的偏移位置越界 */
     {
         return count ? - ENXIO : 0;
     }
@@ -91,7 +97,7 @@ static ssize_t globalmem_write(struct file *filp, const char __user *buf,
     }
 
     /* 用户空间->内核空间 */
-    if (copy_from_user((void *)(dev.mem + p), buf, count))
+    if (copy_from_user((void *)(devp->mem + p), buf, count))
     {
         ret = - EFAULT;
     }
@@ -100,7 +106,7 @@ static ssize_t globalmem_write(struct file *filp, const char __user *buf,
         *ppos += count;
         ret = count;
 
-        printk(KERN_INFO "written %d bytes from %lu\n", count, p);
+        printk(KERN_INFO "written %d bytes to %lu\n", count, p);
     }
 
     return ret;
@@ -109,6 +115,7 @@ static ssize_t globalmem_write(struct file *filp, const char __user *buf,
 static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
 {
     loff_t ret;
+    printk(KERN_INFO "llseek: offset=%lld,orig=%d\n", offset, orig);
     switch(orig)
     {
         case 0:     /* SEEK_SET 从文件开头开始偏移 */
@@ -166,17 +173,39 @@ static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
 static long globalmem_ioctl(struct file *filp, unsigned int cmd, 
         unsigned long arg)
 {
+    struct globalmem_dev *devp = filp->private_data;
+    printk(KERN_INFO "ioctl: cmd=%u, arg=%lu\n", cmd, arg);
     switch (cmd)
     {
         case MEM_CLEAR:
             /* 清除全局内存 */
-            memset(dev.mem, 0, GLOBALMEM_SIZE);
+            memset(devp->mem, 0, GLOBALMEM_SIZE);
             printk(KERN_INFO "globalmem is set to zero\n");
             break;
 
         default:
             return - EINVAL;    /* 其他不支持的命令 */
     }
+    return 0;
+}
+
+/* 文件打开函数 */
+static int globalmem_open(struct inode *inode, struct file *filp)
+{
+    /* 降设备结构体指针赋值给文件私有数据指针 */
+    struct globalmem_dev *devp;
+    printk(KERN_INFO "open: \n");
+
+    devp = container_of(inode->i_cdev, struct globalmem_dev, cdev);
+    filp->private_data = devp;
+
+    return 0;
+}
+
+/* 文件关闭函数 */
+static int globalmem_release(struct inode *inode, struct file *filp)
+{
+    printk(KERN_INFO "release: \n");
     return 0;
 }
 
@@ -187,17 +216,19 @@ static const struct file_operations globalmem_fops =
     .read = globalmem_read,
     .write = globalmem_write,
     .unlocked_ioctl = globalmem_ioctl,
+    .open = globalmem_open,
+    .release = globalmem_release,
 };
 
 /* 初始化并添加cdev结构体 */
-static void globalmem_setup_cdev(void)
+static void globalmem_setup_cdev(struct globalmem_dev *devp, int index)
 {
-    int err, devno = MKDEV(globalmem_major, 0);
+    int err, devno = MKDEV(globalmem_major, index);
 
-    cdev_init(&dev.cdev, &globalmem_fops);
-    dev.cdev.owner = THIS_MODULE;
-    dev.cdev.ops = &globalmem_fops;
-    err = cdev_add(&dev.cdev, devno, 1);
+    cdev_init(&devp->cdev, &globalmem_fops);
+    devp->cdev.owner = THIS_MODULE;
+    devp->cdev.ops = &globalmem_fops;
+    err = cdev_add(&devp->cdev, devno, 1);
     if (err)
     {
         printk(KERN_NOTICE "Error %d adding globalmem", err);
@@ -208,17 +239,18 @@ static void globalmem_setup_cdev(void)
 int globalmem_init(void)
 {
     int result;
+    int i = 0;
     dev_t devno = MKDEV(globalmem_major, 0);
 
     /* 申请字符设备驱动区域 */
     if (globalmem_major)
     {
-        result = register_chrdev_region(devno, 1, "globalmem");
+        result = register_chrdev_region(devno, GLOBALMEM_NUM, "globalmem");
     }
     else
     {
         /* 动态获得主设备号 */
-        result = alloc_chrdev_region(&devno, 0, 1, "globalmem");
+        result = alloc_chrdev_region(&devno, 0, GLOBALMEM_NUM, "globalmem");
         globalmem_major = MAJOR(devno);
     }
     if (result < 0)
@@ -226,14 +258,48 @@ int globalmem_init(void)
         return result;
     }
 
-    globalmem_setup_cdev();
+    /* 动态申请设备结构体的内存 */
+    globalmem_devp = kmalloc(GLOBALMEM_NUM * sizeof(struct globalmem_dev), 
+            GFP_KERNEL);
+    if (!globalmem_devp)    /* 申请失败 */
+    {
+        result = - ENOMEM;
+        goto fail_malloc;
+    }
+    memset(globalmem_devp, 0, GLOBALMEM_NUM * sizeof(struct globalmem_dev));
+
+    for (i = 0; i < GLOBALMEM_NUM; i++)
+    {
+        globalmem_setup_cdev(globalmem_devp + i, i);
+    }
     return 0;
+
+fail_malloc:
+    unregister_chrdev_region(devno, 1);
+    return result;
 }
 
 /* globalmem设备驱动模块卸载函数 */
 void globalmem_exit(void)
 {
-    cdev_del(&dev.cdev);   /*删除cdev结构 */
-    unregister_chrdev_region(MKDEV(globalmem_major, 0), 1); /* 注销这个区域 */
+    int i = 0;
+
+    /*删除cdev结构 */
+    for (i = 0; i < GLOBALMEM_NUM; i++)
+    {
+        cdev_del(&globalmem_devp[i].cdev);   
+    }
+    
+    kfree(globalmem_devp);  /* 释放设备结构体内存 */
+    /* 注销这个区域 */
+    unregister_chrdev_region(MKDEV(globalmem_major, 0), GLOBALMEM_NUM); 
 }
+
+MODULE_AUTHOR("lifuxian");
+MODULE_LICENSE("Dual BSD/GPL");
+
+module_param(globalmem_major, int, S_IRUGO);
+
+module_init(globalmem_init);
+module_exit(globalmem_exit);
 
