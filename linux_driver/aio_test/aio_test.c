8de55034499848d0f2bf69a6ca4f3fa82c9c88ea
lifuxian <lifuxianoa@163.com>
2012-04-27 14:02:09 +0800

    modify: 使用信号作为AIO的通知

--- linux_driver/aio_test/aio_test.c
+++ linux_driver/aio_test/aio_test.c
@@ -27,21 +27,44 @@
 #include <string.h>
 #include <errno.h>
 
-#define BUFF_SIZE   1024
 #define MAX_LIST    2
+#define BUFF_SIZE   1024
 #define FILENAME    "/dev/globalfifo"
-#define FILENAME1   "/dev/globalfifo1"
+
+void aio_completion_handler(int signo, siginfo_t *info, void *context)
+{
+    struct aiocb *req;
+
+    /* 确定是我们需要的信号 */
+    if (SIGIO == info->si_signo)
+    {
+        req = (struct aiocb *)info->si_value.sival_ptr; /* 获得aiocb */
+
+        /* 请求的操作完成了吗？ */
+        if (0 == aio_error(req))
+        {
+            /* 请求的操作完成，获取返回值 */
+            if (aio_return(req) > 0)
+            {
+                /* 获得异步读的返回值 */
+                printf("aio_return, buff=%s\n", (char *)req->aio_buf);
+            }
+            else
+            {
+                /* 读失败，分析errorno */
+                perror("aio_return failed.");
+            }
+
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    int fd, fd1, ret;
-    struct aiocb my_aiocb, my_aiocb1;
-    struct aiocb *cblist[MAX_LIST];
-
-    /* 清零aiocb结构体 */
-    bzero((char *)&my_aiocb, sizeof(struct aiocb));
-    bzero((char *)&my_aiocb1, sizeof(struct aiocb));
-    memset(cblist, 0, sizeof(cblist));
+    int fd, ret;
+    struct aiocb my_aiocb;
+    struct sigaction sig_act;
+    struct aiocb * cblist[MAX_LIST];
 
     fd = open(FILENAME, O_RDONLY);
     if (fd < 0)
@@ -49,72 +72,41 @@ int main(int argc, char *argv[])
         perror("open failed.");
     }
 
+    /* 清零aiocb结构体 */
+    memset(cblist, 0, sizeof(cblist));
+    bzero((char *)&my_aiocb, sizeof(struct aiocb));
+
     /* 为aiocb请求分配数据缓冲区 */
     my_aiocb.aio_buf = malloc(BUFF_SIZE + 1);
     if (!my_aiocb.aio_buf)
     {
         perror("malloc failed.");
     }
+    memset((void *)my_aiocb.aio_buf, 0, BUFF_SIZE + 1);
+
+    /* 设置信号处理函数 */
+    sigemptyset(&sig_act.sa_mask);
+    sig_act.sa_flags = SA_SIGINFO;
+    sig_act.sa_sigaction = aio_completion_handler;
 
     /* 初始化aiocb的成员 */
     my_aiocb.aio_fildes = fd;
     my_aiocb.aio_nbytes = BUFF_SIZE;
     my_aiocb.aio_offset = 0;
-    my_aiocb.aio_lio_opcode = LIO_READ;
-
-    fd1 = open(FILENAME1, O_RDONLY);
-    if (fd1 < 0)
-    {
-        perror("open failed.");
-    }
-
-    /* 为aiocb请求分配数据缓冲区 */
-    my_aiocb1.aio_buf = malloc(BUFF_SIZE + 1);
-    if (!my_aiocb1.aio_buf)
-    {
-        perror("malloc failed.");
-    }
-
-    /* 初始化aiocb的成员 */
-    my_aiocb1.aio_fildes = fd1;
-    my_aiocb1.aio_nbytes = BUFF_SIZE;
-    my_aiocb1.aio_offset = 0;
-    my_aiocb1.aio_lio_opcode = LIO_READ;
+    my_aiocb.aio_sigevent.sigev_notify = SIGEV_SIGNAL;
+    my_aiocb.aio_sigevent.sigev_signo = SIGIO;
+    my_aiocb.aio_sigevent.sigev_value.sival_ptr = &my_aiocb;
 
     cblist[0] = &my_aiocb;
-    cblist[1] = &my_aiocb1;
 
-    /* 发起大量I/O操作 */
-    ret = lio_listio(LIO_WAIT, cblist, MAX_LIST, NULL);
+    /* 将信号月信号处理函数绑定 */
+    ret = sigaction(SIGIO, &sig_act, NULL);
 
-    if ((ret = aio_return(&my_aiocb)) > 0)
-    {
-        /* 获得异步读的返回值 */
-        printf("111 aio_return, ret = %d, buff=%s\n", ret, 
-                (char *)my_aiocb.aio_buf);
-    }
-    else
-    {
-        /* 读失败，分析errorno */
-        perror("111 aio_return failed.");
-    }
-
-    if ((ret = aio_return(&my_aiocb1)) > 0)
-    {
-        /* 获得异步读的返回值 */
-        printf("222 aio_return, ret = %d, buff=%s\n", ret, 
-                (char *)my_aiocb1.aio_buf);
-    }
-    else
-    {
-        /* 读失败，分析errorno */
-        perror("222 aio_return failed.");
-    }
+    ret = aio_read(&my_aiocb);  /* 发出异步读请求 */
+    ret = aio_suspend(cblist, MAX_LIST, NULL);
 
     free((void *)my_aiocb.aio_buf);
-    free((void *)my_aiocb1.aio_buf);
     close(fd);
-    close(fd1);
 
     return 0;
 }
