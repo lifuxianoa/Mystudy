62d082ce21fa03bb22a3b1ed86ac2f6848746ccb
lifuxian <lifuxianoa@163.com>
2012-04-27 15:01:23 +0800

    modify: 使用回调函数作为AIO异步I/O通知机制

--- linux_driver/aio_test/aio_test.c
+++ linux_driver/aio_test/aio_test.c
@@ -31,31 +31,25 @@
 #define BUFF_SIZE   1024
 #define FILENAME    "/dev/globalfifo"
 
-void aio_completion_handler(int signo, siginfo_t *info, void *context)
+void aio_completion_handler(sigval_t sigval)
 {
-    struct aiocb *req;
+    struct aiocb *req = (struct aiocb*)sigval.sival_ptr;
 
-    /* 确定是我们需要的信号 */
-    if (SIGIO == info->si_signo)
+    /* 请求的操作完成了吗？ */
+    if (0 == aio_error(req))
     {
-        req = (struct aiocb *)info->si_value.sival_ptr; /* 获得aiocb */
-
-        /* 请求的操作完成了吗？ */
-        if (0 == aio_error(req))
+        /* 请求的操作完成，获取返回值 */
+        if (aio_return(req) > 0)
         {
-            /* 请求的操作完成，获取返回值 */
-            if (aio_return(req) > 0)
-            {
-                /* 获得异步读的返回值 */
-                printf("aio_return, buff=%s\n", (char *)req->aio_buf);
-            }
-            else
-            {
-                /* 读失败，分析errorno */
-                perror("aio_return failed.");
-            }
-
+            /* 获得异步读的返回值 */
+            printf("aio_return, buff=%s\n", (char *)req->aio_buf);
         }
+        else
+        {
+            /* 读失败，分析errorno */
+            perror("aio_return failed.");
+        }
+
     }
 }
 
@@ -63,7 +57,6 @@ int main(int argc, char *argv[])
 {
     int fd, ret;
     struct aiocb my_aiocb;
-    struct sigaction sig_act;
     struct aiocb * cblist[MAX_LIST];
 
     fd = open(FILENAME, O_RDONLY);
@@ -84,26 +77,27 @@ int main(int argc, char *argv[])
     }
     memset((void *)my_aiocb.aio_buf, 0, BUFF_SIZE + 1);
 
-    /* 设置信号处理函数 */
-    sigemptyset(&sig_act.sa_mask);
-    sig_act.sa_flags = SA_SIGINFO;
-    sig_act.sa_sigaction = aio_completion_handler;
-
     /* 初始化aiocb的成员 */
     my_aiocb.aio_fildes = fd;
     my_aiocb.aio_nbytes = BUFF_SIZE;
     my_aiocb.aio_offset = 0;
-    my_aiocb.aio_sigevent.sigev_notify = SIGEV_SIGNAL;
-    my_aiocb.aio_sigevent.sigev_signo = SIGIO;
+
+    /* 连接AIO请求和线程回调函数 */
+    my_aiocb.aio_sigevent.sigev_notify = SIGEV_THREAD;
+    //my_aiocb.aio_sigevent._sigev_un._sigev_thread._function 
+    //    = aio_completion_handler;
+    my_aiocb.aio_sigevent.sigev_notify_function = aio_completion_handler;
+
+    /* 设置会点函数参数 */
     my_aiocb.aio_sigevent.sigev_value.sival_ptr = &my_aiocb;
+    //my_aiocb.aio_sigevent._sigev_un._sigev_thread._attribute = NULL;
+    my_aiocb.aio_sigevent.sigev_notify_attributes = NULL;
 
     cblist[0] = &my_aiocb;
 
-    /* 将信号月信号处理函数绑定 */
-    ret = sigaction(SIGIO, &sig_act, NULL);
-
     ret = aio_read(&my_aiocb);  /* 发出异步读请求 */
-    ret = aio_suspend(cblist, MAX_LIST, NULL);
+    sleep(10);
+    //ret = aio_suspend(cblist, MAX_LIST, NULL);
 
     free((void *)my_aiocb.aio_buf);
     close(fd);
